3ae2c8f61de32b54f6f5744e2c6d28fcac6a3040
Sam Warren <sjwarren91@gmail.com>
2019-07-24 20:54:12 +1000

    add new gif button and form]

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -34,7 +34,7 @@ function getGifs(){
         var items = response.data;
         console.log(items);
         items.forEach(function(value){
-            var div = $("<div>");
+            var div = $("<div class='card'>");
             var rate = $("<p id='rating'>Rating: " + value.rating.toUpperCase() + "</p>");
             console.log(rate)
             var image = $("<img class='gif' src='" + value.images.fixed_height_still.url + "'>");
@@ -64,7 +64,22 @@ function animateGif(){
 
 }
 
-$(document).ready(function() {
+function newGifButton(){
+    var newButton = $("#gifText").val();
+    $("#gifText").val("");
+    var button = $("<button>");
+    button.attr("type", "button");
+    button.addClass("btn btn-primary m-1");
+    button.text(newButton);
+    button.on("click", getGifs);
+    $(".button-container").append(button);
+}
 
-    $("button").on("click", getGifs);
+$(document).ready(function() {
+    $("#newGif").on("click", function(event){
+        event.preventDefault();
+        newGifButton();
+        
+    });
+    $(".btn-primary").on("click", getGifs);
 });
